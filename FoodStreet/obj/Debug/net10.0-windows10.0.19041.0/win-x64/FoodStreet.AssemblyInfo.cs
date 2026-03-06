02098b35688e1d783b3a968c6b8ad0de98d6294b
Thuan2809 <truongthuan2809@gmail.com>
2026-03-06 15:33:23 +0700

    Tạo giao diện home

--- FoodStreet/obj/Debug/net10.0-windows10.0.19041.0/win-x64/FoodStreet.AssemblyInfo.cs
+++ FoodStreet/obj/Debug/net10.0-windows10.0.19041.0/win-x64/FoodStreet.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodStreet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2238ce806006663b49f558c4a09308168fecbdf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+21ba4aa29f81af20858e1f83875ae14e9f59bc1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodStreet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodStreet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
